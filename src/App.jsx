5180221133a19d1e0f9cce1790a782d1a67c7bc3
NwinNwin <ndnguyen2003@gmail.com>
2022-12-14 16:34:59 -0800

    create dice boxes and roll button to roll dice

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from "react";
 import './App.css';
+import Die from './components/Die';
 
 function App() {
+  const numArray = [0,0,0,0,0
+                    ,0,0,0,0,0];
+  
+  const [die, setDie] = useState(allNewDice())
+
+  function allNewDice(){
+    return numArray.map(ele => Math.floor(Math.random() * 6) + 1)
+  }
+
+  function handleButtonClick(){
+    setDie(prev => allNewDice())
+  }
+
+  const displayArray = die.map(ele => <Die value = {ele}/>)
+  
   return (
-    <main>
-      
+    <main className='main-div'>
+      <div className="die-div">
+      {displayArray}
+      </div>
+
+      <button className="roll-btn" onClick={handleButtonClick}>
+        <h1>Roll</h1>
+      </button>
+
     </main>
   );
 }
